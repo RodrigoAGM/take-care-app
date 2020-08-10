f12a2e088e115609c72a077ba93f7d62098b682c
RodrigoAGM <rod-gm@hotmail.com>
2020-08-10 06:21:57 -0500

    Profile fragment ui implemented

--- app/src/main/java/com/example/takecare/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/takecare/ui/profile/ProfileFragment.kt
@@ -22,9 +22,7 @@ class ProfileFragment : Fragment() {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
         profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
         })
         return root
     }
