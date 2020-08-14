14645df4f88a0bc4be3da163961670108357b2db
RodrigoAGM <rod-gm@hotmail.com>
2020-08-14 01:51:42 -0500

    Add login screen ui

--- app/src/main/java/com/example/takecare/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/takecare/ui/login/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.example.takecare.ui.login
 
+import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.takecare.MainActivity
 import com.example.takecare.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        login_btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
