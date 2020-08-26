20597e8f8c76350469635dd5928628d66a682be6
Enzo Lizama <lizama.enzo@gmail.com>
2020-08-26 17:27:38 -0500

    Adding request & response models

---
+++ app/src/main/java/com/example/takecare/data/api/request/LoginRequest.kt
@@ -0,0 +1,9 @@
+package com.example.takecare.data.api.request
+
+
+/**
+ * Created by Enzo Lizama Paredes on 8/26/20.
+ * Contact: lizama.enzo@gmail.com
+ */
+
+data class LoginRequest(val username: String, val password: String)
