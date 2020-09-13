b0ccdeee962ecdecac0597a69dda38600643b9f3
RodrigoAGM <rod-gm@hotmail.com>
2020-09-13 15:45:55 -0500

    Add change password to menu and add icons

--- app/src/main/java/com/example/takecare/MainActivity.kt
+++ app/src/main/java/com/example/takecare/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.takecare
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ImageSpan
+import android.util.Log
 import android.view.MenuItem
 import android.widget.PopupMenu
 import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
@@ -15,9 +20,12 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.takecare.data.repository.LoginRepository
 import com.example.takecare.ui.login.LoginActivity
+import com.example.takecare.ui.profile.UpdatePasswordActivity
 import com.example.takecare.utils.PatientUtil
 import com.example.takecare.utils.PreferenceHelper
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,7 +54,9 @@ class MainActivity : AppCompatActivity() {
         toolbar_profile.setOnClickListener {
             val popupMenu = PopupMenu(this, toolbar_profile)
             popupMenu.menuInflater.inflate(R.menu.toolbar_menu, popupMenu.menu)
-
+            popupMenu.menu.add(0, 1, 1, menuIconWithText(getDrawable(R.drawable.ic_lock_black_24dp)!!, getString(R.string.menu_change_pass)))
+            popupMenu.menu.add(0, 2, 2, menuIconWithText(getDrawable(R.drawable.ic_logout_black_24dp)!!, getString(R.string.menu_logout)))
+            
             popupMenu.setOnMenuItemClickListener {
                 MenuItemClick().onMenuItemClick(it)
             }
@@ -64,15 +74,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private inner class MenuItemClick : PopupMenu.OnMenuItemClickListener{
+
         override fun onMenuItemClick(item: MenuItem?): Boolean {
 
-            if(item!!.title == getString(R.string.menu_logout)){
-                viewModel.logout()
+            when(item!!.title.toString().trim()){
+                getString(R.string.menu_logout) -> {
+                    viewModel.logout()
+                }
+
+                getString(R.string.menu_change_pass) -> {
+                    val intent = Intent(applicationContext, UpdatePasswordActivity::class.java)
+                    startActivity(intent)
+                }
             }
 
             return false
         }
+    }
 
+    private fun menuIconWithText(icon: Drawable, title: String): CharSequence? {
+        icon.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
+        val sb = SpannableString("    $title")
+        val imageSpan = ImageSpan(icon, ImageSpan.ALIGN_BOTTOM)
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return sb
     }
 
     private fun setupViewModel() {
