3d3ce4bd8d5fc738e84dd0333ca42033ddcac4a6
RodrigoAGM <rod-gm@hotmail.com>
2020-09-25 20:35:40 -0500

    Add get treatments response

--- app/src/main/java/com/example/takecare/data/api/response/UserResponse.kt
+++ app/src/main/java/com/example/takecare/data/api/response/UserResponse.kt
@@ -1,6 +1,7 @@
 package com.example.takecare.data.api.response
 
 import com.example.takecare.model.Patient
+import com.example.takecare.model.Treatment
 import com.google.gson.annotations.SerializedName
 
 data class RegisterResponse(
@@ -17,4 +18,10 @@ data class UpdateResponse(
 data class UpdatePasswordResponse(
     val success: Boolean,
     val message:String
+)
+
+data class GetTreatmentResponse(
+    val success: Boolean,
+    @SerializedName("data")
+    val treatments: List<Treatment>
 )
