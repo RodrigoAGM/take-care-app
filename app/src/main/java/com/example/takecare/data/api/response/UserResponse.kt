29f47cfa26a9bc69d9976c936d858100c214deaf
RodrigoAGM <rod-gm@hotmail.com>
2020-09-25 20:36:30 -0500

    Add get quotes response

--- app/src/main/java/com/example/takecare/data/api/response/UserResponse.kt
+++ app/src/main/java/com/example/takecare/data/api/response/UserResponse.kt
@@ -1,6 +1,7 @@
 package com.example.takecare.data.api.response
 
 import com.example.takecare.model.Patient
+import com.example.takecare.model.Quote
 import com.example.takecare.model.Treatment
 import com.google.gson.annotations.SerializedName
 
@@ -24,4 +25,10 @@ data class GetTreatmentResponse(
     val success: Boolean,
     @SerializedName("data")
     val treatments: List<Treatment>
+)
+
+data class GetQuotesResponse(
+    val success: Boolean,
+    @SerializedName("data")
+    val quotes: List<Quote>
 )
