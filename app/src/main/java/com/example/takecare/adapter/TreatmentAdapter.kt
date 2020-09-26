f092b28a9536230e12f6b22f69c2e402f05bb3bf
RodrigoAGM <rod-gm@hotmail.com>
2020-09-25 23:00:05 -0500

    Treatmend card fix

--- app/src/main/java/com/example/takecare/adapter/TreatmentAdapter.kt
+++ app/src/main/java/com/example/takecare/adapter/TreatmentAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.takecare.adapter
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.takecare.R
 import com.example.takecare.model.Treatment
@@ -29,18 +32,22 @@ class TreatmentAdapter(private var treatments: ArrayList<Treatment>)  : Recycler
     }
 
     inner class HistoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val date = view.treatment_card_date
-        val indications = view.treatment_card_indications
-        val medication = view.treatment_card_medication
-        val psychiatrist = view.treatment_card_psychiatrist
+        private val date = view.treatment_card_date
+        private val indications = view.treatment_card_indications
+        private val medication = view.treatment_card_medication
+        private val psychiatrist = view.treatment_card_psychiatrist
 
         @SuppressLint("SetTextI18n")
         fun bind(treatment: Treatment) {
 
             indications.text = treatment.description
+
             val days = ( treatment.quantity.toDouble() / (24.0/ treatment.frequency.toDouble()) )
-            medication.text = "${treatment.quantity} pastillas de ${treatment.medicineName} cada ${treatment.frequency} por ${ceil(days)} días"
+            medication.text = "${treatment.quantity} pastillas de ${treatment.medicineName} cada ${treatment.frequency} por ${ceil(days).toInt()} días"
+
             date.text = "Fecha: ${treatment.creationDate.dropLast(14)}"
+            date.isEnabled = (treatment.status == 1)
+
             psychiatrist.text = "Psiquiatra: ${treatment.psychiatristName} ${treatment.psychiatristLastName}"
         }
     }
