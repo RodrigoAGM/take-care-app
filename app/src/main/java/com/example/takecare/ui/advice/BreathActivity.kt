111589fa1f06e5d40dae9ef253cce47c4627858b
RodrigoAGM <rod-gm@hotmail.com>
2020-10-01 02:09:16 -0500

    Add video on breath methods screen

--- app/src/main/java/com/example/takecare/ui/advice/BreathActivity.kt
+++ app/src/main/java/com/example/takecare/ui/advice/BreathActivity.kt
@@ -3,18 +3,22 @@ package com.example.takecare.ui.advice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.view.View
+import android.widget.VideoView
 import com.example.takecare.R
 import kotlinx.android.synthetic.main.activity_breath.*
 
 class BreathActivity : AppCompatActivity() {
 
-    private var breathing = false
+    //private var breathing = false
+    private lateinit var videoView: VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_breath)
 
-        val chronometer = breath_inner_circle_counter
+        videoView = breath_video
+        /*val chronometer = breath_inner_circle_counter
 
         breath_inner_circle.setOnClickListener {
             if(!breathing){
@@ -28,8 +32,26 @@ class BreathActivity : AppCompatActivity() {
                 breathing = false
                 breath_inner_circle_text.text = getString(R.string.breath_circle_text)
             }
+        }*/
+        configureVideo()
+
+        videoView.setOnClickListener {
+            if(videoView.isPlaying){
+                videoView.pause()
+            }else{
+                videoView.start()
+            }
+        }
+
+        videoView.setOnPreparedListener {
+            it.isLooping = true
+            breath_progressBar.visibility = View.INVISIBLE
         }
     }
 
+    private fun configureVideo(){
+        videoView.setVideoPath(getString(R.string.breath_video_url))
+        videoView.seekTo(1)
+    }
 
 }
